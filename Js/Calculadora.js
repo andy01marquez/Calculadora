363001056d2b1a56c3ae891c32aeecf5af48b46d
andy01marquez <andrea-marfer@hotmail.com>
2022-11-18 12:04:30 -0600

    Segundo commit

--- Js/Calculadora.js
+++ Js/Calculadora.js
@@ -1,37 +1,80 @@
 
 
 var x="0";
-var x1=1;
+var x_borrar=1;
 var coma=0;
 var op="no";
+var valor = 0;
+var num=0;
 
-pantalla = document.getElementById("textoPantalla");
+pantalla=document.getElementById("textoPantalla"); 
 
-function numero(xx) { 
+function numero(value) { 
+   if (x=="0" || x_borrar==1  ) 
+   { 
+   pantalla.innerHTML=value; 
+   x=value; 
 
-if (x=="0" || xi==1  ) {	
-    pantalla.innerHTML=xx; 
-    x=xx; 
-    if (xx==".") { 
-       pantalla.innerHTML="0.";
-       x=xx;
-       coma=1; 
+   if (value==".") 
+   { 
+      pantalla.innerHTML="0."; 
+      x=value;
+      coma=1;
+   }
 
-    if (xx=="." && coma==0) { 
-           pantalla.innerHTML+=xx;
-           x+=xx;
-           coma=1; 
-       }
-      
-    else if (xx=="." && coma==1) {
+  }
+
+  else 
+  { 
+      if (value=="." && coma==0) 
+      { 
+          pantalla.innerHTML+=value;
+          x+=value;
+          coma=1;
+      }
     
-    } 
-      
-       else {
-           pantalla.innerHTML+=xx;
-           x+=xx
-       }
-    }
-    xi=0 
- }
+      else if (value=="." && coma==1) 
+      {
+
+      } 
+   	 
+      else 
+      {
+          pantalla.innerHTML+=value;
+          x+=value;
+      } 
+   }
+   x_borrar=0; 
+}
+
+function operacion(tipo) 
+{
+   ni=x;
+   op=tipo;
+   x_borrar=1;
+}	
+
+function igual() 
+{
+   if (op=="no") 
+   {
+      pantalla.innerHTML=x;	
+   }
+   
+   else 
+   { 
+      op=ni+op+x; 
+      resultado = eval(op);
+      pantalla.innerHTML = resultado;
+      x=sol; 
+      op="no"; 
+      x_borrar=1;
+   }       
+}
+
+function c() 
+{
+   pantalla = document.getElementById('textoPantalla');
+   pantalla.innerHTML = "0";
+   x_borrar=1;
 }
